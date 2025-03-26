757e17992a0cbe94d258cdfef804ffc85e26126a
zihluwang <really@zihlu.wang>
2025-03-26 11:00:00 +0800

    feat: added tailwindcss

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+import { BrowserRouter, Route, Routes } from "react-router"
+import { store } from "@/store"
 import "./index.css"
-import App from "./App.tsx"
+import { ProtectedRoute } from "@/components/protected-route"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={null}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
