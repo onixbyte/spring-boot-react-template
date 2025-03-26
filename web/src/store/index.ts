a731c1c320f34eb32cf1d223a2ab226ef419ee1e
zihluwang <really@zihlu.wang>
2025-03-26 10:51:06 +0800

    feat: added auth store

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { useDispatch, useSelector } from "react-redux"
+import authReducer from "./auth-slice.ts"
 
 export const store = configureStore({
   reducer: {
-
-  }
+    auth: authReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
